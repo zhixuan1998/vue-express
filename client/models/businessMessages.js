16613e0a328eaf5eeb7c69b1ad962ad015934e9c
seowzhixuan199800126 <zhixuan.seow@gmail.com>
2024-03-29 01:22:41 +0800

    Updated: business messages

--- client/models/businessMessages.js
+++ client/models/businessMessages.js
@@ -1,4 +1,14 @@
 export default {
-    required: () => "Please fill in this field."
-    
+    validations: {
+        required: () => "This field is required.",
+        invalidFormat: (obj) => `Invalid ${obj} format.`,
+        passwordFormat: () => "Password must be 8 - 20 alphanumeric characters, at least an uppercase and a symbol.",
+    },
+    label: {
+        loginUsername: () => "Phone number / Username /Email",
+        username: () => "Username",
+        password: () => "Password",
+        email: () => "email",
+        phoneNumber: () => "Phone Number",
+    }
 }
