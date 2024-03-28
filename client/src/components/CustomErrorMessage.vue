20624541d2b2e2ab1df4a9928d2ea3ef7de5efe5
seowzhixuan199800126 <zhixuan.seow@gmail.com>
2024-03-29 01:22:22 +0800

    Added: custom error message component

---
+++ client/src/components/CustomErrorMessage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="error-message-item">
+    <font-awesome-icon class="icon" :icon="faTriangleExclamation" />
+    <p class="message">{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+defineProps({
+  showIcon: Boolean,
+  message: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+<style lang="scss" scoped>
+.error-message-item {
+  display: flex;
+  align-items: center;
+
+  .icon {
+    margin-right: 5px;
+  }
+
+  .message {
+    flex-grow: 1;
+    margin: 0;
+    font-weight: 500;
+  }
+}
+</style>
