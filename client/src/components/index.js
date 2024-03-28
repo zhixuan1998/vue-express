f7ff065a3bb38be64ec1b16965f43951f1818320
seowzhixuan199800126 <zhixuan.seow@gmail.com>
2024-03-29 01:23:42 +0800

    Updated: export custom component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,6 +4,7 @@ import CustomHeader from "./CustomHeader.vue";
 import CustomSearchBox from "./CustomSearchBox.vue";
 import CustomSeparator from "./CustomSeparator.vue";
 import CustomFormControl from "./CustomFormControl.vue";
+import CustomErrorMessage from "./CustomErrorMessage.vue";
 
 export default {
     CustomForm,
@@ -11,5 +12,6 @@ export default {
     CustomHeader,
     CustomSearchBox,
     CustomSeparator,
-    CustomFormControl
+    CustomFormControl,
+    CustomErrorMessage
 }
