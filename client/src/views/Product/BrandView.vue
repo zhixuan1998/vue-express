cd052bf56b314730966818dfdb76bd739b9f0fd6
seowzhixuan199800126 <zhixuan.seow@gmail.com>
2024-04-15 01:52:58 +0800

    Reverted: format route path logic'

--- client/src/views/Product/BrandView.vue
+++ client/src/views/Product/BrandView.vue
@@ -1,82 +1,29 @@
 <template>
   <div class="content-section">
     <div class="content-label">{{ $messages.title.brands() }} - {{ brand?.name ?? '' }}</div>
-    <custom-mini-item-carousel :items="brands" labelField="name" @select-item="setBrand" />
+    <custom-mini-item-carousel :items="brands" labelField="name" @select-item="goToBrand" />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, inject, watch } from 'vue';
-import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router';
+import { onMounted, ref, inject, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
 const router = useRouter();
 const $repositories = inject('repositories');
 const $messages = inject('messages');
 
-const brandId = defineModel('brandId');
 const brands = ref([]);
-const brand = ref('');
-
-onBeforeRouteUpdate((to) => setBrand(to.params));
 
 onMounted(async () => {
   const brandResults = await $repositories.brandRepository.getAll({});
   brands.value = brandResults?.data?.data?.results ?? [];
-
-  setBrand();
 });
 
-function setBrand(params = route.params) {
-  const [paramName, index] = params.brandId.split('.');
+const brand = computed(() => brands.value.find((o) => o.brandId === route.params.brandId));
 
-  if (paramName && !isNaN(index)) {
-    setBrandWithIndex({ paramName, index });
-  } else {
-    setBrandWithBrandId(params.brandId);
-  }
-
-  if (brand.value) {
-    brandId.value = brand.value.brandId;
-  }
-}
-
-function setBrandWithBrandId(brandId) {
-  if (!brands.value.length || !brandId) return;
-
-  let index = 0;
-
-  for (let i = 0; i < brands.value.length; i++) {
-    const found = brands.value[i].brandId === brandId;
-
-    if (found) {
-      index = i;
-      brand.value = brands.value[i];
-      break;
-    }
-  }
-
-  if (!brand.value) {
-    return router.push({ name: 'Product' });
-  }
-
-  const formattedName = brand.value.name.replaceAll(' ', '-');
-
-  router.push({
-    name: 'ProductBrand',
-    params: { brandId: `${formattedName}.${index}` }
-  });
-}
-
-function setBrandWithIndex({ paramName, index }) {
-  if (!brands.value.length || (!paramName && isNaN(index))) return;
-
-  paramName = paramName.replaceAll('-', ' ');
-
-  brand.value = brands.value[index];
-
-  if (paramName !== brand.value?.name) {
-    return router.push({ name: 'Product' });
-  }
+function goToBrand(item) {
+  router.push({ name: 'ProductBrand', params: { brandId: item.brandId } });
 }
 </script>
--- client/src/views/Product/CategoryView.vue
+++ client/src/views/Product/CategoryView.vue
@@ -1,84 +1,31 @@
 <template>
   <div class="content-section">
     <div class="content-label">{{ $messages.title.categories() }} - {{ category?.name || '' }}</div>
-    <custom-mini-item-carousel :items="categories" labelField="name" @select-item="setCategory" />
+    <custom-mini-item-carousel :items="categories" labelField="name" @select-item="goToCategory" />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, inject } from 'vue';
-import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router';
+import { onMounted, ref, inject, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
 const router = useRouter();
 const $repositories = inject('repositories');
 const $messages = inject('messages');
 
-const categoryId = defineModel('categoryId');
 const categories = ref([]);
-const category = ref('');
-
-onBeforeRouteUpdate((to) => setCategory(to.params));
 
 onMounted(async () => {
   const categoryResults = await $repositories.categoryRepository.getAll({});
   categories.value = categoryResults?.data?.data?.results ?? [];
-
-  setCategory(route.params, true);
 });
 
-function setCategory(params, replaceRoute = false) {
-  const [paramName, index] = params.categoryId.split('.');
+const category = computed(() =>
+  categories.value.find((o) => o.categoryId === route.params.categoryId)
+);
 
-  if (paramName && !isNaN(index)) {
-    setCategoryWithIndex({ paramName, index });
-  } else {
-    setCategoryWithCategoryId(params.categoryId, replaceRoute);
-  }
-
-  if (category.value) {
-    categoryId.value = category.value.categoryId; 
-  }
-}
-
-function setCategoryWithCategoryId(categoryId, replaceRoute) {
-  if (!categories.value.length || !categoryId) return;
-
-  let index = 0;
-
-  for (let i = 0; i < categories.value.length; i++) {
-    const found = categories.value[i].categoryId === categoryId;
-
-    if (found) {
-      index = i;
-      category.value = categories.value[i];
-      break;
-    }
-  }
-
-  if (!category.value) {
-    return router.push({ name: 'Product' });
-  }
-
-  const formattedName = category.value.name.replaceAll(' ', '-');
-
-  const config = {
-    name: 'ProductCategory',
-    params: { categoryId: `${formattedName}.${index}` }
-  };
-
-  replaceRoute ? router.replace(config) : router.push(config);
-}
-
-function setCategoryWithIndex({ paramName, index }) {
-  if (!categories.value.length || (!paramName && isNaN(index))) return;
-
-  paramName = paramName.replaceAll('-', ' ');
-
-  category.value = categories.value[index];
-
-  if (paramName !== category.value?.name) {
-    return router.push({ name: 'Product' });
-  }
+function goToCategory(item) {
+  router.push({ name: 'ProductCategory', params: { categoryId: item.categoryId } });
 }
 </script>
